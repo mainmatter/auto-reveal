29e6db70b7a127e558800f67802157aa2bd80bec
Florian Pichler <fp@ylk.gd>
2024-02-02 15:31:14 +0100

    Initial commit

---
+++ bin/auto-reveal.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+import { createServer } from 'vite';
+import { ViteEjsPlugin as viteEjsPlugin } from 'vite-plugin-ejs';
+import { getTheme, getTitle } from '../lib/utils.js';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+const argv = process.argv.slice(2);
+const cwd = process.cwd();
+
+getTheme();
+
+async function start() {
+  const server = await createServer({
+    configFile: false,
+    root: path.join(__dirname, '..', 'src'),
+    server: {
+      port: 1337,
+    },
+    plugins: [
+      viteEjsPlugin({
+        title: getTitle(),
+      }),
+    ],
+    resolve: {
+      alias: {
+        slides: path.join(cwd, 'slides'),
+        '@theme': getTheme(),
+      },
+    },
+  });
+
+  await server.listen();
+  server.printUrls();
+  server.bindCLIShortcuts({ print: true });
+}
+
+async function build() {}
+
+if (!argv[0] || argv[0] === 'start') {
+  start();
+} else if (argv[0] === 'build') {
+  // Build for production
+}
---
+++ src/main.js
@@ -0,0 +1,46 @@
+import Reveal from 'reveal.js';
+import Highlight from 'reveal.js/plugin/highlight/highlight.esm.js';
+import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+import Notes from 'reveal.js/plugin/notes/notes.esm.js';
+
+import 'reveal.js/dist/reveal.css';
+import '@theme';
+
+const markdownFiles = import.meta.glob('slides/*.md', {
+  as: 'raw',
+  eager: true,
+});
+
+const sortedMarkdownFiles = Object.entries(markdownFiles).sort(([a], [b]) => {
+  const regex = /slides\/(\d+)-/;
+  const [, aNumb] = regex.exec(a);
+  const [, bNumb] = regex.exec(b);
+
+  return Number(aNumb) - Number(bNumb);
+});
+
+const sections = sortedMarkdownFiles.map(
+  ([, content]) => `
+	<section 
+		data-markdown
+		data-separator="^\n___\n$"
+		data-separator-vertical="^\n---\n$"
+		data-separator-notes="^Note:"
+	>
+		<textarea data-template>${content}</textarea>
+	</section >
+`
+);
+
+document.querySelector('.slides').innerHTML = sections.join('');
+
+const deck = new Reveal();
+
+deck.initialize({
+  hash: true,
+  width: 1280,
+  height: 960,
+  margin: 0.1,
+  highlight: {},
+  plugins: [Markdown, Highlight, Notes],
+});
