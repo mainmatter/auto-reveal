4d64f5c2aebf56b5a3c209de7c66e6ed4681a93e
Chris Manson <chris@manson.ie>
2024-04-29 14:39:25 +0100

    add a default highlight theme to the css
    It would be nice if we could figure out a way for a theme to provide this without needing to lock in versions of highlight or reveal... something to discuss

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
 import Notes from 'reveal.js/plugin/notes/notes.esm.js';
 
 import 'reveal.js/dist/reveal.css';
+import 'reveal.js/plugin/highlight/monokai.css'
 import '@theme';
 
 const markdownFiles = import.meta.glob('slides/*.md', {
