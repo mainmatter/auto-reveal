362fc67a392e6e25aca9438ae3422256bb187325
Florian Pichler <fp@ylk.gd>
2025-03-24 11:47:17 +0100

    Make port configurable
    Closes #54

--- tests/program.test.js
+++ tests/program.test.js
@@ -22,6 +22,19 @@ describe('auto-reveal CLI', () => {
 		expect(start).toBeCalledTimes(1);
 	});
 
+	test('Using auto-reveal start with a port passes the port to the start function', async () => {
+		await program.parseAsync([
+			'node',
+			'auto-reveal',
+			'start',
+			'--port',
+			'8080',
+		]);
+
+		expect(start).toBeCalledTimes(1);
+		expect(start.mock.lastCall[0].port).toBe('8080');
+	});
+
 	test('Using auto-reveal build triggers the build function', async () => {
 		await program.parseAsync(['node', 'auto-reveal', 'build']);
 
