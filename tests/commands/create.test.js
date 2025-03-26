224875813ce3c676c625ec7aa7e6842d31b6d01f
Florian Pichler <fp@ylk.gd>
2025-03-26 18:59:50 +0100

    Re-use code from add command for create

--- tests/commands/create.test.js
+++ tests/commands/create.test.js
@@ -1,3 +1,4 @@
+import path from 'node:path';
 import { execa } from 'execa';
 import fixturify from 'fixturify';
 import { describe, expect, it } from 'vitest';
@@ -15,14 +16,19 @@ describe('create command tests', () => {
 		const fixtures = fixturify.readSync(cwd);
 
 		expect(result.exitCode).to.equal(0);
-		expect(result.stdout).to.include('\nauto-reveal\n');
+		expect(result.stdout).toStrictEqual(
+			`\nauto-reveal\n\n  Setting up your presentation in \n  ${cwd}\n\n  Hint: Create new slides by running "auto-reveal add". \n  Hint: Create new vertical slides by adding "---" inside your slide markdown file.\n`,
+		);
 		expect(fixtures).toStrictEqual({
 			public: {},
 			slides: {
-				'000.md': expect.stringMatching(/^# tmp-/),
+				'000.md': expect.stringMatching(/.*/),
 			},
 			'package.json': expect.stringMatching(/.*/),
 		});
+		expect(fixtures.slides['000.md']).toStrictEqual(
+			`# ${path.basename(cwd)}\n\nNote:\n\nThis note is only visible to the presenter.\n`,
+		);
 		expect(JSON.parse(fixtures['package.json'])).toStrictEqual({
 			name: expect.stringMatching(/^tmp-/),
 			version: '0.0.0',
