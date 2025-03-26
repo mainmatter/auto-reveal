02f6b2cf12de27a9065e90d7ee7cdd032a8d2915
Florian Pichler <fp@ylk.gd>
2025-03-26 16:26:26 +0100

    Create makeFolder test helperbased on fixturify

--- tests/helpers.js
+++ tests/helpers.js
@@ -1,12 +1,18 @@
 import fs from 'node:fs';
 import path from 'node:path';
+import fixturify from 'fixturify';
 import { Project } from 'fixturify-project';
 import { globby } from 'globby';
+import tmp from 'tmp';
+
+tmp.setGracefulCleanup();
 
 export async function makeProject({ files = {}, theme } = {}) {
 	const project = new Project('test-app', '0.0.1', {
 		files,
 	});
+
+	project.files = files;
 
 	// setup the current auto-reveal as a dev dependency to link its bin
 	project.linkDevDependency('auto-reveal', { baseDir: '.', resolveName: '.' });
@@ -21,6 +27,15 @@ export async function makeProject({ files = {}, theme } = {}) {
 	await project.write();
 
 	return { cwd: project.baseDir, project };
+}
+
+export async function makeFolder({ files } = {}) {
+	const _tmp = tmp.dirSync({ unsafeCleanup: true });
+	const baseDir = fs.realpathSync(_tmp.name);
+
+	fixturify.writeSync(baseDir, files);
+
+	return { cwd: baseDir };
 }
 
 export async function getFileContents(glob, cwd) {
